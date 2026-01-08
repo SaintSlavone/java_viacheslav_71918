6aaf0531fe02faa9aa5aaeb66c424a4b0f1f6ea7
Viacheslav <viakostee@gmail.com>
2026-01-08 11:43:16 +0100

    added ProductService, ProductMapper, ProductRequest and ProductResponse classes

--- First-Rest-Api-Spring/src/main/java/pl/edu/vistula/firstrestapispring/product/api/response/ProductResponse.java
+++ First-Rest-Api-Spring/src/main/java/pl/edu/vistula/firstrestapispring/product/api/response/ProductResponse.java
@@ -1,4 +1,16 @@
-package pl.edu.vistula.firstrestapispring.api.response;
+package pl.edu.vistula.firstrestapispring.product.api.response;
 
 public class ProductResponse {
+    private final Long id;
+
+    private final String name;
+
+    public ProductResponse(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
